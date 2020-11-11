e367b1e7b7a0c704f195da08a4068f3cbfcf25e6
a-khoirunnufus <a.khoirunnufus@gmail.com>
2020-11-11 20:46:30 +0700

    view hompage desktop bagian header

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -1,1 +1,65 @@
-<h1>PORTIR ADDAARIAT</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PORTIR ADDAARIAT</title>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<!-- header section -->
+	<div class="header">
+		<div class="tagline-bg">
+			<div class="container d-flex flex-column h-100">
+				<div>				
+					<svg id="svg-header-brand" width="387" height="122" viewBox="0 0 387 122" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<style>
+							text{
+								font-size: 1.25rem;
+								color: rgba(0,0,0,.9);
+							}
+						</style>
+						<path d="M0 -10.90406C2.64401 27.9981 0 110.808 77.6505 120.396C151.022 129.454 165.5 89.2939 313.524 110.808C366.293 118.478 384.495 14.7985 387 -38L0 -5.90406Z" fill="white"/>
+						<image x="45.5" y="13" href="assets/img/logo_portiraddariat.jpg" height="72" width="72" />
+						<text x="123" y="54" fill="black">PORTIR ADDAARIAT</text>
+					</svg>
+					<nav class="navbar navbar-expand-lg navbar-light bg-alpha">
+						<a class="navbar-brand hidden-elm" href="#">
+							<img id="logo-header" src="assets/img/logo_portiraddariat.jpg">
+							<span>PORTIR ADDAARIAT</span>
+						</a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+						</button>
+
+						<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							<div class="navbar-nav ml-auto">
+								<a class="nav-item nav-link" href="#">KEGIATAN KAMI</a>
+								<a class="nav-item nav-link" href="#">TENTANG KAMI</a>
+								<a class="nav-item nav-link" href="#">DONASI</a>
+								<a class="nav-item nav-link" href="#">BANTUAN</a>
+							</div>
+						</div>
+					</nav>
+				</div>
+				<div class="flex-fill d-flex">
+					<div class="jumbotron transparent align-self-center col-6">
+						<h1 class="display-4 text-white">Hello, world!</h1>
+						<p class="lead text-white">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					</div>
+				</div>
+			</div>
+			<svg id="svg-header-bor-btm" width="1366" height="193" viewBox="0 0 1366 193" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M-56 107.5C5.83333 77.5 167.9 24.7 321.5 53.5C513.5 89.5 753 103.5 911.5 53.5C1070 3.49997 1375.5 -55.5 1484 107.5C1570.8 237.9 1459.83 190.5 1393.5 150.5H-56V107.5Z" fill="white"/>
+			</svg>
+
+		</div>
+	</div>
+<!-- header section end -->
+
+<!-- kegiatan section -->
+
+<!-- kegiatan section end -->
+
+<div style="width: 100%; height: 500px">
+</body>
+</html>
